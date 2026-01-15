8dde1b6411c492e2eed9cb7b6381be3a5fe90dc0
tekraj <tekrajpant224@gmail.com>
2026-01-15 15:11:40 +0000

    fix: urls

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('Products/', views.product_list, name='product_list'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('about-us/', views.about, name='about'),
+    path('contact-us/', views.contact, name='contact'),
+    path('Testimonials/', views.testimonials, name='testimonials'),
 ]
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
 from cms.models import Page
+from catalog.models import Product
 
 def home(request):
     pages = Page.objects.all()
     return render(request, "home.html", {"pages": pages})
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, "product-list.html", {"products": products})
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, "product.html", {"product": product})
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def testimonials(request):
+    return render(request, "testimonials.html")
